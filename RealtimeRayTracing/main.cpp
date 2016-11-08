1229966a8eab6c5b11d9f468b787c9d060edb763
soadzoor <soadzoor@gmail.com>
2016-11-08 01:22:17 +0100

    Fixed a bug on linux where every key was triggered twice when pressed.

--- RealtimeRayTracing/main.cpp
+++ RealtimeRayTracing/main.cpp
@@ -17,23 +17,23 @@ void exitProgram()
 	system("pause");
 }
 
-int main(int argc, char* args[])
+int main( int argc, char* args[] )
 {
 	// állítsuk be, hogy kilépés elõtt hívja meg a rendszer az exitProgram() függvényt - Kérdés: mi lenne enélkül?
-	atexit(exitProgram);
+	atexit( exitProgram );
 
 	//
 	// 1st step: Initialize SDL
 	//
 
 	// Turn on graphics subsystem, check for errors
-	if (SDL_Init(SDL_INIT_VIDEO) == -1)
+	if ( SDL_Init( SDL_INIT_VIDEO ) == -1 )
 	{
 		// Write out errors, then exit.
 		std::cout << "[init SDL]Error while initializing SDL: " << SDL_GetError() << std::endl;
 		return 1;
 	}
-
+			
 	//
 	// 2. lépés: állítsuk be az OpenGL-es igényeinket, hozzuk létre az ablakunkat, indítsuk el az OpenGL-t
 	//
@@ -42,20 +42,19 @@ int main(int argc, char* args[])
 
 	// beállíthatjuk azt, hogy pontosan milyen OpenGL context-et szeretnénk létrehozni - ha nem tesszük, akkor
 	// automatikusan a legmagasabb elérhetõ verziójút kapjuk
-	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
-	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	//SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    //SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
 	// állítsuk be, hogy hány biten szeretnénk tárolni a piros, zöld, kék és átlátszatlansági információkat pixelenként
-	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
-	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
-	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE,         32);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE,            8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,          8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE,           8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE,          8);
 	// duplapufferelés
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,		1);
 	// mélységi puffer hány bites legyen
-	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,          24);
 
 	// antialiasing - ha kell
 	//SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS,  1);
@@ -63,33 +62,33 @@ int main(int argc, char* args[])
 
 	// hozzuk létre az ablakunkat
 	SDL_Window *win = 0;
-	int width = 320;
-	int height = 240;
-	win = SDL_CreateWindow("Hello SDL&OpenGL!",		// az ablak fejléce
-		100,						// az ablak bal-felsõ sarkának kezdeti X koordinátája
-		100,						// az ablak bal-felsõ sarkának kezdeti Y koordinátája
-		width,						// ablak szélessége
-		height,						// és magassága
-		SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);			// megjelenítési tulajdonságok
+	int width = 800;
+	int height = 600;
+    win = SDL_CreateWindow( "Hello SDL&OpenGL!",		// az ablak fejléce
+							100,						// az ablak bal-felsõ sarkának kezdeti X koordinátája
+							100,						// az ablak bal-felsõ sarkának kezdeti Y koordinátája
+							width,						// ablak szélessége
+							height,						// és magassága
+							SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);			// megjelenítési tulajdonságok
 
 
-																				// ha nem sikerült létrehozni az ablakot, akkor írjuk ki a hibát, amit kaptunk és lépjünk ki
-	if (win == 0)
+	// ha nem sikerült létrehozni az ablakot, akkor írjuk ki a hibát, amit kaptunk és lépjünk ki
+    if (win == 0)
 	{
 		std::cout << "[Creating window]Error while initializing window: " << SDL_GetError() << std::endl;
-		return 1;
-	}
+        return 1;
+    }
 
 	//
 	// 3rd step: creating OpenGL context
 	//
 
-	SDL_GLContext	context = SDL_GL_CreateContext(win);
-	if (context == 0)
+	SDL_GLContext	context	= SDL_GL_CreateContext(win);
+    if (context == 0)
 	{
 		std::cout << "[Creating OGL context]Error while initializing SDL: " << SDL_GetError() << std::endl;
-		return 1;
-	}
+        return 1;
+    }	
 
 	// Vsync: ON
 
@@ -97,24 +96,24 @@ int main(int argc, char* args[])
 
 	// start GLEW
 	GLenum error = glewInit();
-	if (error != GLEW_OK)
+	if ( error != GLEW_OK )
 	{
 		std::cout << "[GLEW] Error while initializing!" << std::endl;
 		return 1;
 	}
 
 	// Request OpenGL version number
-	int glVersion[2] = { -1, -1 };
-	glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]);
-	glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]);
+	int glVersion[2] = {-1, -1}; 
+	glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]); 
+	glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]); 
 	std::cout << "Running OpenGL " << glVersion[0] << "." << glVersion[1] << std::endl;
 
 	printf("Vendor (%s), Renderer (%s)\n", glGetString(GL_VENDOR), glGetString(GL_RENDERER));
 
-	if (glVersion[0] == -1 && glVersion[1] == -1)
+	if ( glVersion[0] == -1 && glVersion[1] == -1 )
 	{
 		SDL_GL_DeleteContext(context);
-		SDL_DestroyWindow(win);
+		SDL_DestroyWindow( win );
 
 		std::cout << "[Creating OGL context] Error while creating OpenGL context! Some options in SDL_GL_SetAttribute(...) calls might be wrong." << std::endl;
 
@@ -133,7 +132,7 @@ int main(int argc, char* args[])
 	bool quit = false;
 	// message to be processed
 	SDL_Event ev;
-
+	
 	// instance of the app
 	CMyApp app;
 	if (!app.Init())
@@ -148,7 +147,7 @@ int main(int argc, char* args[])
 	while (!quit)
 	{
 		// while there are messages to process, process them:
-		while (SDL_PollEvent(&ev))
+		while ( SDL_PollEvent(&ev) )
 		{
 			switch (ev.type)
 			{
@@ -168,11 +167,11 @@ int main(int argc, char* args[])
 						app.Resize(width, height);
 					}
 				}
-
-				app.KeyboardDown(ev.key);
+					
+				if (ev.key.repeat == 0) app.KeyboardDown(ev.key);
 				break;
 			case SDL_KEYUP:
-				app.KeyboardUp(ev.key);
+				if (ev.key.repeat == 0) app.KeyboardUp(ev.key);
 				break;
 			case SDL_MOUSEBUTTONDOWN:
 				app.MouseDown(ev.button);
@@ -187,7 +186,7 @@ int main(int argc, char* args[])
 				app.MouseMove(ev.motion);
 				break;
 			case SDL_WINDOWEVENT:
-				if (ev.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
+				if ( ev.window.event == SDL_WINDOWEVENT_SIZE_CHANGED )
 				{
 					app.Resize(ev.window.data1, ev.window.data2);
 					std::cout << "Resolution changed to: " << ev.window.data1 << "x" << ev.window.data2 << std::endl;
@@ -229,7 +228,7 @@ int main(int argc, char* args[])
 	app.Clean();
 
 	SDL_GL_DeleteContext(context);
-	SDL_DestroyWindow(win);
+	SDL_DestroyWindow( win );
 
 	return 0;
 }
